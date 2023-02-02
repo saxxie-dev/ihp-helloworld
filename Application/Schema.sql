e43452682e4e26fa4bccc14f975265118374aaf7
csachse <csachse@tableau.com>
2023-02-02 15:33:45 +0000

    transfer changes off computer

--- Application/Schema.sql
+++ Application/Schema.sql
@@ -10,7 +10,8 @@ CREATE TABLE posts (
     title TEXT NOT NULL,
     body TEXT NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
-    updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
+    "comment" UUID
 );
 CREATE INDEX posts_created_at_index ON posts (created_at);
 CREATE TRIGGER update_posts_updated_at BEFORE UPDATE ON posts FOR EACH ROW EXECUTE FUNCTION set_updated_at_to_now();
@@ -24,3 +25,4 @@ CREATE TABLE comments (
 CREATE INDEX comments_post_id_index ON comments (post_id);
 CREATE INDEX comments_created_at_index ON comments (created_at);
 ALTER TABLE comments ADD CONSTRAINT comments_ref_post_id FOREIGN KEY (post_id) REFERENCES posts (id) ON DELETE CASCADE;
+ALTER TABLE posts ADD CONSTRAINT posts_ref_comment FOREIGN KEY ("comment") REFERENCES comments (id) ON DELETE NO ACTION;
